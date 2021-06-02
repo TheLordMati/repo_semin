50800248076c3d135e65b6c927e437da06160434
TheLordMati <70500103+TheLordMati@users.noreply.github.com>
2021-06-02 01:29:32 -0300

    Add files via upload

---
+++ punto_3/punto_3/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblhora = New System.Windows.Forms.Label()
+        Me.lblfecha = New System.Windows.Forms.Label()
+        Me.horafecha = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'lblhora
+        '
+        Me.lblhora.AutoSize = True
+        Me.lblhora.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblhora.ForeColor = System.Drawing.Color.Aqua
+        Me.lblhora.Location = New System.Drawing.Point(174, 82)
+        Me.lblhora.Name = "lblhora"
+        Me.lblhora.Size = New System.Drawing.Size(331, 108)
+        Me.lblhora.TabIndex = 0
+        Me.lblhora.Text = "Label1"
+        '
+        'lblfecha
+        '
+        Me.lblfecha.AutoSize = True
+        Me.lblfecha.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblfecha.ForeColor = System.Drawing.Color.SlateGray
+        Me.lblfecha.Location = New System.Drawing.Point(137, 190)
+        Me.lblfecha.Name = "lblfecha"
+        Me.lblfecha.Size = New System.Drawing.Size(131, 42)
+        Me.lblfecha.TabIndex = 1
+        Me.lblfecha.Text = "Label1"
+        '
+        'horafecha
+        '
+        Me.horafecha.Enabled = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(42, Byte), Integer), CType(CType(42, Byte), Integer), CType(CType(42, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(736, 314)
+        Me.Controls.Add(Me.lblfecha)
+        Me.Controls.Add(Me.lblhora)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Reloj"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblhora As Label
+    Friend WithEvents lblfecha As Label
+    Friend WithEvents horafecha As Timer
+End Class
---
+++ punto_3/punto_3/Form1.vb
@@ -0,0 +1,7 @@
+﻿Imports System.DateTime
+Public Class Form1
+    Private Sub horafecha_Tick(sender As Object, e As EventArgs) Handles horafecha.Tick
+        lblhora.Text = DateTime.Now.ToLongTimeString()
+        lblfecha.Text = DateTime.Now.ToLongDateString()
+    End Sub
+End Class
