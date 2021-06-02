1e80fe59a30c8db668b4f62291a3d5c8342b9db8
TheLordMati <70500103+TheLordMati@users.noreply.github.com>
2021-06-02 19:46:17 -0300

    Add files via upload

---
+++ cronometro/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Dim StopWatch As New Diagnostics.Stopwatch
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim elapsed As TimeSpan = Me.StopWatch.Elapsed
+        Label1.Text = String.Format("{0:00}:{1:00}:{2:00}:{3:00}", Math.Floor(elapsed.TotalHours), elapsed.Minutes, elapsed.Seconds, elapsed.Milliseconds)
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Timer1.Start()
+        Me.StopWatch.Start()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Timer1.Stop()
+        Me.StopWatch.Stop()
+    End Sub
+
+    Private Sub Button3_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        ListBox1.Items.Add(Label1.Text)
+    End Sub
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Me.StopWatch.Reset()
+        Label1.Text = "00:00:00:00"
+        ListBox1.Items.Clear()
+    End Sub
+End Class
