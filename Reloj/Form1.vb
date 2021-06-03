448c6b7df4e0208a37e4ac5c5b8fb9c7aa32c4d2
TheLordMati <70500103+TheLordMati@users.noreply.github.com>
2021-06-02 23:13:47 -0300

    Delete Reloj directory

--- Reloj/Form1.vb
+++
@@ -1,7 +0,0 @@
-﻿Imports System.DateTime
-Public Class Form1
-    Private Sub horafecha_Tick(sender As Object, e As EventArgs) Handles horafecha.Tick
-        lblhora.Text = DateTime.Now.ToLongTimeString()
-        lblfecha.Text = DateTime.Now.ToLongDateString()
-    End Sub
-End Class
